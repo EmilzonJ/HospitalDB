7cd036f5785cb461fd990a987a0fe12684e6decc
CarlosSu02 <carlossuplei@gmail.com>
2022-08-12 23:59:08 -0600

    Add: fn de dep_emp que lista los empleados de un departamento

---
+++ sql/functions/departamentos_empleados/fn_departamento_empleados.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION fn_departamento_empleados(vw_departamento_id INT)
+RETURNS TABLE(empleado_id INT, nombre VARCHAR(100), apellido VARCHAR(100), ocupacion VARCHAR(100)) 
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    
+    IF NOT EXISTS (SELECT * FROM departamentos_empleados WHERE departamento_id = vw_departamento_id) THEN
+        
+        RAISE NOTICE 'No hay empleados en el departamento "%".', vw_departamento_id;
+		        
+    ELSE
+
+        RETURN QUERY SELECT 
+            e.id, e.nombres, e.apellidos, oc.descripcion
+        FROM 
+            empleados e
+        INNER JOIN 
+            ocupaciones oc ON e.ocupacion_id = oc.id
+        INNER JOIN 
+            departamentos_empleados d_e ON e.id = d_e.empleado_id
+        WHERE 
+            d_e.departamento_id = vw_departamento_id;
+
+    END IF;
+    
+END$$;
