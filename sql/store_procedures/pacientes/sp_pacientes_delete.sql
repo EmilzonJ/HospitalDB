0c0f05cfadf5e9053155aa4f9fc699508d1abb35
Sary Nog <sary.noguera@unah.hn>
2022-08-13 19:26:40 -0600

    Add: delete sp de pacientes

---
+++ sql/store_procedures/pacientes/sp_pacientes_delete.sql
@@ -0,0 +1,9 @@
+﻿CREATE OR REPLACE PROCEDURE sp_pacientes_delete(_id INT)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    DELETE FROM pacientes WHERE id = _id;
+    RAISE NOTICE 'Paciente eliminado con exito';
+END
+$$
