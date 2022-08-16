f4db3192d9e3ec13425b53d72934036698415609
Andre <apalvaradol@outlook.com>
2022-08-16 12:37:20 -0600

    Add: calls de hospital

---
+++ sql/functions/hospital/calls/call_hospital.sql
@@ -0,0 +1,3 @@
+CALL sp_hospital_insert('Hospital Juan Luis', 'San Juan de Opoa', '25516798');
+CALL sp_hospital_update(1, 'Hospital Luis Celeste', 'San Juan de Opoa', '25516798');
+CALL sp_hospital_delete(1);
