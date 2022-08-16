2426488d0785ad582a417fa6cfea9ec6cce56773
Andre <apalvaradol@outlook.com>
2022-08-16 13:23:27 -0600

    Add: calls de socios

---
+++ sql/functions/socios/calls/call_socios.sql
@@ -0,0 +1,3 @@
+CALL sp_socios_insert(2, 'Italo Gonzalo','Martinez Carcamo', '+50498765438', 'italogmc@yahoo.com', '0801-1969-09044');
+CALL sp_socios_update(2, 'Italo Gonzalo','Martinez Carcamo', '+50497653090', 'italogmc@yahoo.com', '0801-1969-09044');
+CALL sp_socios_delete(7);
