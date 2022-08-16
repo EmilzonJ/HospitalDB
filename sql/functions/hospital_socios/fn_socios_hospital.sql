566435b033dc491300a2781cb915dc53a5a5ec90
Andre <apalvaradol@outlook.com>
2022-08-16 10:54:10 -0600

    Add: lista de hospitales afiliados a un socio hospital_socios

---
+++ sql/functions/hospital_socios/fn_socios_hospital.sql
@@ -0,0 +1,30 @@
+--Listar en que hospitales estan afiliados los socios
+CREATE OR REPLACE FUNCTION fn_socios_hospital(_socio_id INT)
+    RETURNS TABLE
+            (
+                hospital_id INT,
+                nombres     VARCHAR(100),
+				direccion VARCHAR(255)
+            )
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+
+    IF NOT EXISTS(SELECT * FROM hospital_socios WHERE socio_id = _socio_id) THEN
+
+        RAISE NOTICE 'Esta persona no está afiliada a ningun hospital "%".', _socio_id;
+    END IF;
+
+        RETURN QUERY SELECT h.id,
+                            h.nombre,
+                            h.direccion
+                     FROM hospital h
+                              INNER JOIN
+                          hospital_socios h_s ON h.id = h_s.hospital_id
+                     WHERE h_s.socio_id = _socio_id;
+
+
+
+END
+$$;
