a749caa5c66f9572772b59236fa4c1dea6b26f4c
Sary Nog <sary.noguera@unah.hn>
2022-08-15 22:41:22 -0600

    Add: fn de ocupaciones

---
+++ sql/functions/ocupaciones/fn_ocupaciones.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION fn_ocupaciones(_id INT)
+    RETURNS TABLE
+            (
+                OCUPACION_ID          INT,
+                OCUPACION_DESCRIPCION VARCHAR(100)
+            )
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF NOT EXISTS(SELECT * FROM OCUPACIONES WHERE ocupacion_id = _id) THEN
+
+        RAISE NOTICE 'La ocupacion no existe "%".', _id;
+    END IF;
+    RETURN QUERY SELECT ocupaciones.id,
+                        ocupaciones.descripcion
+                 FROM ocupaciones
+                 WHERE ocupaciones.id = _id;
+END;
+$$;
