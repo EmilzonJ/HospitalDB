555f5d0c0a4c9d739d1144e38448ec078078d0b5
Sary Nog <sary.noguera@unah.hn>
2022-08-15 21:13:23 -0600

    Add: calls de tipos socios

---
+++ sql/store_procedures/tipos_socios/calls/sp_calls.sql
@@ -0,0 +1,11 @@
+﻿--CAll sp insert de Tipos de socios
+CALL sp_tipos_socios_insert(
+        'Naturales'
+    );
+--CAll sp update de Tipos de socios
+CALL sp_tipos_socios_update(
+        '2',
+        'Juridicos'
+    );
+--CAll sp delete de Tipos de socios
+CALL sp_tipos_socios_delete('2');
