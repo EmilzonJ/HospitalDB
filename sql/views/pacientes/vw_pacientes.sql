feb32436c04ebdbeb123686103cbd7e2bf6b7550
Sary Nog <sary.noguera@unah.hn>
2022-08-15 19:40:31 -0600

    Add: view de pacientes

---
+++ sql/views/pacientes/vw_pacientes.sql
@@ -0,0 +1,17 @@
+﻿CREATE OR REPLACE VIEW vw_pacientes AS
+SELECT p.id AS pacientes_id,
+       p.nombres,
+       p.apellidos,
+       p.genero,
+       p.direccion,
+       p.fecha_nacimiento,
+       p.departamento_id,
+       p.empleado_id,
+       p.estado,
+       p.tipo_sangre
+
+FROM pacientes p
+         INNER JOIN
+     departamentos d ON p.departamento_id = d.id
+         INNER JOIN
+     empleados e ON p.empleado_id = e.id;
