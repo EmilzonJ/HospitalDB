c3a7cf180e2716d128c703c698c93c3552284de6
Sary Nog <sary.noguera@unah.hn>
2022-08-15 22:30:13 -0600

    Add: fn de pacientes tutores

---
+++ sql/functions/pacientes_tutores/fn_pacientes_tutores.sql
@@ -0,0 +1,30 @@
+﻿CREATE OR REPLACE FUNCTION fn_pacientes_tutores(_paciente_id INT)
+    RETURNS TABLE
+            (
+                TUTOR_ID  INT,
+                NOMBRES   VARCHAR(100),
+                APELLIDOS VARCHAR(100)
+            )
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+
+    IF NOT EXISTS(SELECT * FROM pacientes_tutores WHERE paciente_id = _paciente_id) THEN
+
+        RAISE NOTICE 'No hay tutores para el pacientes "%".', _paciente_id;
+
+    ELSE
+
+        RETURN QUERY SELECT t.id,
+                            t.nombres,
+                            t.apellidos
+                     FROM tutores t
+                              INNER JOIN
+                          pacientes_tutores p_t ON t.id = p_t.tutor_id
+                     WHERE p_t.paciente_id = _paciente_id;
+
+    END IF;
+
+END
+$$;
