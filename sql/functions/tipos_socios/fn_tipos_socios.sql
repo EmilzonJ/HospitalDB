8183de79d9f3a078c52671ba7f0c6d42976f8c3e
Sary Nog <sary.noguera@unah.hn>
2022-08-15 22:49:32 -0600

    Add: fn de tipos socios

---
+++ sql/functions/tipos_socios/fn_tipos_socios.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION fn_tipos_socios(_id INT)
+    RETURNS TABLE
+            (
+                TIPOS_SOCIOS_ID          INT,
+                TIPOS_SOCIOS_DESCRIPCION VARCHAR(100)
+            )
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF NOT EXISTS(SELECT * FROM tipos_socios WHERE tipos_socios_id = _id) THEN
+
+        RAISE NOTICE 'El tipo de socio no existe "%".', _id;
+    END IF;
+    RETURN QUERY SELECT tipos_socios.id,
+                        tipos_socios.descripcion
+                 FROM tipos_socios
+                 WHERE tipos_socios.id = _id;
+END;
+$$;
