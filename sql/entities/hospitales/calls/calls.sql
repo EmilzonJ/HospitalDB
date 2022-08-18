7e2b6fb238e01945151157767fad2bc13713294f
EmilzonJ <emilzonujero@gmail.com>
2022-08-17 23:12:56 -0600

    Reformat calls.sql

--- sql/entities/hospitales/calls/calls.sql
+++ sql/entities/hospitales/calls/calls.sql
@@ -1,9 +1,18 @@
---calls stored procedures 
+--calls stored procedures
+CALL sp_hospital_insert(
+        'Hospital Juan Luis',
+        'San Juan de Opoa',
+        '25516798');
 
-cALL sp_hospital_insert('Hospital Juan Luis', 'San Juan de Opoa', '25516798');
-CALL sp_hospital_update(1, 'Hospital Luis Celeste', 'San Juan de Opoa', '25516798');
+CALL sp_hospital_update(
+        1,
+        'Hospital Luis Celeste',
+        'San Juan de Opoa',
+        '25516798');
+
 CALL sp_hospital_delete(1);
 
 --view hospital select
-SELECT * FROM public.vw_hospitales
+SELECT *
+FROM public.vw_hospitales
 LIMIT 5
