14f5f2d1bbddf486303c09d0f34c64e3a716a4e7
Sary Nog <sary.noguera@unah.hn>
2022-08-17 22:22:52 -0600

    Edit: Carpeta calls

--- sql/entities/pacientes/calls/calls.sql
+++ sql/entities/pacientes/calls/calls.sql
@@ -1,14 +1,17 @@
-﻿--CALL insert de pacientes
+﻿SELECT * FROM vw_pacientes;
+--Select fn de Pacientes
+SELECT * FROM fn_pacientes('1');
+--CALL insert de pacientes
 CALL sp_pacientes_insert('Karla',
-    'Ramirez',
-    'F',
-    '2000-10-10',
-    '0401-2000-00678',
-    'Santa Rosa de Copan',
-    3,
-    1,
-    'Critico',
-    'O+');
+                         'Ramirez',
+                         'F',
+                         '2000-10-10',
+                         '0401-2000-00678',
+                         'Santa Rosa de Copan',
+                         3,
+                         1,
+                         'Critico',
+                         'O+');
 
 --CALL update de pacientes
 CALL sp_pacientes_update('2',
