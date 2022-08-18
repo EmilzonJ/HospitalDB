eb0cd2c7bc92ed8ebfc168b4586414077ad03ed1
Andre <apalvaradol@outlook.com>
2022-08-17 22:17:49 -0600

    Fixed: reagrupacion calls

--- sql/entities/hospitales/calls/call_hospital.sql
+++ sql/entities/hospitales/calls/call_hospital.sql
@@ -1,3 +1,9 @@
-CALL sp_hospital_insert('Hospital Juan Luis', 'San Juan de Opoa', '25516798');
+--calls stored procedures 
+
+ALL sp_hospital_insert('Hospital Juan Luis', 'San Juan de Opoa', '25516798');
 CALL sp_hospital_update(1, 'Hospital Luis Celeste', 'San Juan de Opoa', '25516798');
 CALL sp_hospital_delete(1);
+
+--view hospital select
+SELECT * FROM public.vw_hospitales
+LIMIT 5
--- sql/entities/socios/calls/call_socios.sql
+++ sql/entities/socios/calls/call_socios.sql
@@ -1,3 +1,9 @@
+--CALLS stored prodecures
+
 CALL sp_socios_insert(2, 'Italo Gonzalo','Martinez Carcamo', '+50498765438', 'italogmc@yahoo.com', '0801-1969-09044');
 CALL sp_socios_update(2, 'Italo Gonzalo','Martinez Carcamo', '+50497653090', 'italogmc@yahoo.com', '0801-1969-09044');
 CALL sp_socios_delete(1);
+
+--select socios views
+SELECT * FROM public.vw_socios
+LIMIT 5
