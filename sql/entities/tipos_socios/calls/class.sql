e85e114705aecc057c4246ee4fb35a3672572e66
Sary Nog <sary.noguera@unah.hn>
2022-08-17 22:40:24 -0600

    Edit: Class de tipos socios

--- sql/entities/tipos_socios/calls/class.sql
+++ sql/entities/tipos_socios/calls/class.sql
@@ -1,11 +1,16 @@
-﻿--CAll sp insert de Tipos de socios
+﻿SELECT * FROM vw_tipos_socios;
+
+--Select fn de tipos de socios
+SELECT * FROM fn_tipos_socios(1);
+
+--Call sp insert de tipos de socios
 CALL sp_tipos_socios_insert(
         'Naturales'
     );
 --CAll sp update de Tipos de socios
 CALL sp_tipos_socios_update(
-        '2',
+        1,
         'Juridicos'
     );
 --CAll sp delete de Tipos de socios
-CALL sp_tipos_socios_delete('2');
+CALL sp_tipos_socios_delete(1);
