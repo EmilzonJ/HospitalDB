2fedd36efd1760123c1b50fade8014d29acf987c
Andre <apalvaradol@outlook.com>
2022-08-17 22:20:25 -0600

    Fixed: error en calls de hospital

--- sql/entities/hospitales/calls/call_hospital.sql
+++ sql/entities/hospitales/calls/call_hospital.sql
@@ -1,6 +1,6 @@
 --calls stored procedures 
 
-ALL sp_hospital_insert('Hospital Juan Luis', 'San Juan de Opoa', '25516798');
+cALL sp_hospital_insert('Hospital Juan Luis', 'San Juan de Opoa', '25516798');
 CALL sp_hospital_update(1, 'Hospital Luis Celeste', 'San Juan de Opoa', '25516798');
 CALL sp_hospital_delete(1);
 
