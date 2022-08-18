0846fc3b4fee0c81a51251ef4f5323b553e3732b
Sary Nog <sary.noguera@unah.hn>
2022-08-17 22:45:24 -0600

    Edit: Calls de pacientes tutores

---
+++ sql/entities/pacientes_tutores/calls/calls.sql
@@ -0,0 +1,8 @@
+﻿SELECT * FROM vw_pacientes_tutores.sql;
+ 
+--Select fn de pacientes tutores
+SELECT * FROM fn_pacientes_tutores(1);
+
+--Call sp insert de pacientes tutores
+CALL sp_pac_tut_insert(1,
+                       1);
