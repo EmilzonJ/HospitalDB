21841153cc226211523702960786aa58803d82df
EmilzonJ <emilzonujero@gmail.com>
2022-08-18 00:43:54 -0600

    Refactor VW departamentos

--- sql/entities/departamentos/views/vw_departamentos.sql
+++ sql/entities/departamentos/views/vw_departamentos.sql
@@ -1,11 +1,11 @@
 CREATE OR REPLACE VIEW vw_departamentos AS
-SELECT d.id          AS departamento_id,
-       d.nombre      AS departamento_nombre,
-       d.edificio    AS departamento_edificio,
-       d.descripcion AS departamento_descripcion,
-       h.id          AS hospital_id,
-       h.nombre      AS hospital_nombre,
-       h.direccion   AS hospital_direccion
+SELECT d.id,
+       d.nombre,
+       d.edificio,
+       d.descripcion,
+       h.id        AS hospital_id,
+       h.nombre    AS hospital_nombre,
+       h.direccion AS hospital_direccion
 FROM departamentos d
          INNER JOIN
-     hospital h ON d.hospital_id = h.id;
+     hospitales h ON d.hospital_id = h.id;
